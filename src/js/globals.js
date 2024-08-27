232f44a8021ee22fdd8fd0efa42fbe16a24d27e5
nitesh-e33 <nitesh.varfa@droom.in>
2024-08-27 11:44:29 +0530

    OP-1 || Remove all unnecessary packages

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,29 +1,27 @@
-import '../js/adminlte.min.js'
-
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-import '../js/select2.full.min.js'
-
-
-// DataTables  & Plugins
-import '../plugins/datatables/jquery.dataTables.min.js'
-import '../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'
-import '../plugins/datatables-buttons/js/dataTables.buttons.min.js'
-import '../plugins/datatables-buttons/js/buttons.bootstrap4.min.js'
-import '../plugins/datatables-buttons/js/buttons.colVis.min.js'
-import '../plugins/datatables-responsive/js/dataTables.responsive.min.js'
-import '../plugins/datatables-responsive/js/responsive.bootstrap4.min.js'
-import '../plugins/jszip/jszip.min.js'
-import '../plugins/pdfmake/pdfmake.min.js'
-import '../plugins/pdfmake/vfs_fonts.js'
-import '../plugins/datatables-buttons/js/buttons.html5.min.js'
-import '../plugins/datatables-buttons/js/buttons.print.min.js'
+import '../js/adminlte.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../js/select2.full.min.js';
 
 // import '../plugins/jquery/jquery.min.js'
 // import '../plugins/jquery-ui/jquery-ui.min.js'
-// // $.widget.bridge('uibutton', $.ui.button)
-// Bootstrap 4
+
+// // Bootstrap 4
 // import '../plugins/bootstrap/js/bootstrap.bundle.min.js'
+
+// DataTables & Plugins
+// import '../plugins/datatables/jquery.dataTables.min.js';
+// import '../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js';
+// // import '../plugins/datatables-buttons/js/dataTables.buttons.min.js';
+// import '../plugins/datatables-buttons/js/buttons.bootstrap4.min.js';
+// import '../plugins/datatables-buttons/js/buttons.colVis.min.js';
+// import '../plugins/datatables-responsive/js/dataTables.responsive.min.js';
+// import '../plugins/datatables-responsive/js/responsive.bootstrap4.min.js';
+// import '../plugins/jszip/jszip.min.js';
+// import '../plugins/pdfmake/pdfmake.min.js';
+// import '../plugins/pdfmake/vfs_fonts.js';
+// import '../plugins/datatables-buttons/js/buttons.html5.min.js';
+// import '../plugins/datatables-buttons/js/buttons.print.min.js';
+
 // // daterangepicker
 // import '../plugins/moment/moment.min.js'
 // // import '../plugins/daterangepicker/daterangepicker.js'
